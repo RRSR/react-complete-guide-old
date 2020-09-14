7fa7dcaadacd9a1474e0f22ea2bdfa2740b3cc9a
Raj Rajeshwar Singh Rathore <rajrajeshwarsingh.rathore@practo.com>
2020-09-14 21:03:11 +0530

    Added a list of person details as the state of the App component.

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,21 @@ import './App.css';
 import Person from "./Person/Person";
 
 class App extends Component {
+    state = {
+        personList: [
+            {name: 'Raj', age: 20},
+            {name: 'Hari', age: 29},
+            {name: 'Jai', age: 33}
+        ]
+    }
+
     render() {
         return (
             <div className="App">
                 <h1>Hi, I'm a React App</h1>
-                <Person name="Raj" age="13"/>
-                <Person name="Jai" age="21">My hobbies : Gardening</Person>
-                <Person name="Om" age="32"/>
+                <Person name={this.state.personList[0].name} age={this.state.personList[0].age}/>
+                <Person name={this.state.personList[1].name} age={this.state.personList[1].age}/>
+                <Person name={this.state.personList[2].name} age={this.state.personList[2].age}/>
             </div>
         );
     }
