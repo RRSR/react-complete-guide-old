c5822cb5f5d09f5784f9a01045635fc603e39bee
Raj Rajeshwar Singh Rathore <rajrajeshwarsingh.rathore@practo.com>
2020-09-15 13:11:35 +0530

    Migrated the Class-Component App to Functional-Component app.

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,43 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Person from "./Person/Person";
 
-class App extends Component {
-    state = {
+const app = props => {
+
+    const [personState, setPersonState] = useState({
         personList: [
             {name: 'Raj', age: 20},
             {name: 'Hari', age: 29},
             {name: 'Jai', age: 33}
         ],
         country: 'India'
-    }
+    });
 
-    switchNameHandler = () => {
-        this.setState({
+    const [countryState, setCountryState] = useState({
+        country: 'India'
+    })
+
+    const switchNameHandler = () => {
+        setPersonState({
             personList: [
                 {name: 'Raj Rathore', age: 101},
                 {name: 'Hari', age: 29},
                 {name: 'Jai Singh', age: 21}
             ]
-        })
+        });
     }
 
-    render() {
-        return (
-            <div className="App">
-                <h1>Hi, I'm a React App</h1>
-                <button onClick={this.switchNameHandler}>Switch Name</button>
-                <Person name={this.state.personList[0].name} age={this.state.personList[0].age}/>
-                <Person name={this.state.personList[1].name} age={this.state.personList[1].age}/>
-                <Person name={this.state.personList[2].name} age={this.state.personList[2].age}/>
-                <label>{this.state.country}</label>
-            </div>
-        );
-    }
+    return (
+        <div className="App">
+            <h1>Hi, I'm a React App</h1>
+            <button onClick={switchNameHandler}>Switch Name</button>
+            <Person name={personState.personList[0].name} age={personState.personList[0].age}/>
+            <Person name={personState.personList[1].name} age={personState.personList[1].age}/>
+            <Person name={personState.personList[2].name} age={personState.personList[2].age}/>
+            <label>Person State : {personState.country}</label><br/>
+            <label>Country State : {countryState.country}</label>
+        </div>
+    );
 }
 
-export default App;
+export default app;
