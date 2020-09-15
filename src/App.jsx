18f18eb880ffeae0b1a8ae4c0c1483edea0ce872
Raj Rajeshwar Singh Rathore <rajrajeshwarsingh.rathore@practo.com>
2020-09-15 11:58:59 +0530

    Added an event handler to update the state on the click of a button.

--- src/App.jsx
+++ src/App.jsx
@@ -8,16 +8,29 @@ class App extends Component {
             {name: 'Raj', age: 20},
             {name: 'Hari', age: 29},
             {name: 'Jai', age: 33}
-        ]
+        ],
+        country: 'India'
+    }
+
+    switchNameHandler = () => {
+        this.setState({
+            personList: [
+                {name: 'Raj Rathore', age: 101},
+                {name: 'Hari', age: 29},
+                {name: 'Jai Singh', age: 21}
+            ]
+        })
     }
 
     render() {
         return (
             <div className="App">
                 <h1>Hi, I'm a React App</h1>
+                <button onClick={this.switchNameHandler}>Switch Name</button>
                 <Person name={this.state.personList[0].name} age={this.state.personList[0].age}/>
                 <Person name={this.state.personList[1].name} age={this.state.personList[1].age}/>
                 <Person name={this.state.personList[2].name} age={this.state.personList[2].age}/>
+                <label>{this.state.country}</label>
             </div>
         );
     }
