d1ac15b4fc9bb3eea52ed37bb2330fd20af6410c
Raj Rajeshwar Singh Rathore <rajrajeshwarsingh.rathore@practo.com>
2020-09-16 22:18:33 +0530

    Reverting back to Class-Component App from Functional-Component app.

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,39 @@
-import React, {useState} from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Person from "./Person/Person";
 
-const app = props => {
-
-    const [personState, setPersonState] = useState({
+class App extends Component {
+    state = {
         personList: [
             {name: 'Raj', age: 20},
             {name: 'Hari', age: 29},
             {name: 'Jai', age: 33}
         ],
         country: 'India'
-    });
-
-    const [countryState, setCountryState] = useState({
-        country: 'India'
-    })
-
-    const switchNameHandler = () => {
-        setPersonState({
-            personList: [
-                {name: 'Raj Rathore', age: 101},
-                {name: 'Hari', age: 29},
-                {name: 'Jai Singh', age: 21}
-            ]
-        });
     }
 
-    return (
-        <div className="App">
-            <h1>Hi, I'm a React App</h1>
-            <button onClick={switchNameHandler}>Switch Name</button>
-            <Person name={personState.personList[0].name} age={personState.personList[0].age}/>
-            <Person name={personState.personList[1].name} age={personState.personList[1].age}/>
-            <Person name={personState.personList[2].name} age={personState.personList[2].age}/>
-            <label>Person State : {personState.country}</label><br/>
-            <label>Country State : {countryState.country}</label>
-        </div>
-    );
+    switchNameHandler = () => {
+        this.setState({
+            personList: [
+                {name: 'Raj Rathore', age: 101},
+                {name: 'Hari', age: 91},
+                {name: 'Jai Singh', age: 21}
+            ]
+        })
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <h1>Hi, I'm a React App</h1>
+                <button onClick={this.switchNameHandler}>Switch Name</button>
+                <Person name={this.state.personList[0].name} age={this.state.personList[0].age}/>
+                <Person name={this.state.personList[1].name} age={this.state.personList[1].age}/>
+                <Person name={this.state.personList[2].name} age={this.state.personList[2].age}/>
+                <label>Person State : {this.state.country}</label><br/>
+            </div>
+        );
+    }
 }
 
-export default app;
+export default App;
