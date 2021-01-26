6bab5b2e54fb521466ebc5507b37a6c82a3dc587
Raj Rajeshwar Singh <rrsr1312@gmail.com>
2021-01-26 20:11:14 +0530

    Updated formatting

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from "./Person/Person";
 
 class App extends Component {
     state = {
         personList: [
-            {id: 101, name: 'Raj', age: 20},
-            {id: 102, name: 'Hari', age: 29},
-            {id: 103, name: 'Jai', age: 33}
+            { id: 101, name: 'Raj', age: 20 },
+            { id: 102, name: 'Hari', age: 29 },
+            { id: 103, name: 'Jai', age: 33 }
         ],
         country: 'India',
         showPersons: false
@@ -24,7 +24,7 @@ class App extends Component {
         // const persons = this.state.personList.slice(); Mutable as it changes the original list itself because it's just a reference.
         const persons = [...this.state.personList]; // Immutable as it creates a new copy using spread operator.
         persons.splice(personIndex, 1);
-        this.setState({personList: persons})
+        this.setState({ personList: persons })
     }
 
     nameChangeHandler = (event, id) => {
@@ -42,12 +42,12 @@ class App extends Component {
         const personListCopy = [...this.state.personList];
         personListCopy[personIndex] = personCopy
 
-        this.setState({personList: personListCopy})
+        this.setState({ personList: personListCopy })
     };
 
     togglePersonsHandler = () => {
         const doesShow = this.state.showPersons;
-        this.setState({showPersons: !doesShow});
+        this.setState({ showPersons: !doesShow });
     }
 
     render() {
@@ -57,13 +57,13 @@ class App extends Component {
             persons = (
                 <div>
                     {this.state.personList.map((person, index) => {
-                            return <Person
-                                myClick={() => this.deletePersonHandler(index)}
-                                name={person.name}
-                                age={person.age}
-                                key={person.id}
-                                myChange={(event) => this.nameChangeHandler(event, person.id)}/>
-                        }
+                        return <Person
+                            myClick={() => this.deletePersonHandler(index)}
+                            name={person.name}
+                            age={person.age}
+                            key={person.id}
+                            myChange={(event) => this.nameChangeHandler(event, person.id)} />
+                    }
                     )}
                 </div>
             );
